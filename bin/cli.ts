94ae26e79deb4b08fb8fd5ead263d483414ace72
Chris <chrisbreuer93@gmail.com>
2025-01-25 13:59:43 +0100

    chore: wip

--- bin/cli.ts
+++ bin/cli.ts
@@ -6,13 +6,24 @@ const cli = new CAC('httx')
 
 cli
   .command('[method] [url] [...items]', 'Make an HTTP request')
-  .option('-v, --verbose', 'Enable verbose output')
   .option('-j, --json', 'Send JSON data')
   .option('-f, --form', 'Send form-encoded data')
   .option('-m, --multipart', 'Send multipart form data')
+  .option('-d, --download', 'Download mode')
+  .option('-F, --follow', 'Follow redirects', { default: true })
+  .option('-a, --auth <auth>', 'Authentication (username:password)')
+  .option('-t, --timeout <timeout>', 'Request timeout in milliseconds')
+  .option('-v, --verbose', 'Enable verbose output')
   .action(async (method, url, items, options) => {
     debugLog('cli', `Executing ${method} ${url}`, options.verbose)
     // ...
+  })
+
+cli
+  .command('completion', 'Generate shell completion script')
+  .action(() => {
+    // TODO: Implement shell completion generation
+    console.log('Shell completion not implemented yet')
   })
 
 cli.command('version', 'Show the version of the Reverse Proxy CLI').action(() => {
